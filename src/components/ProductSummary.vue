712212eac117327d8ef9ceebfe9346a55972171f
st170001 <st170001@stud.uni-stuttgart.de>
2023-12-18 21:47:59 +0100

    Better frontend links

--- src/components/ProductSummary.vue
+++ src/components/ProductSummary.vue
@@ -1,34 +1,21 @@
 <template>
-    <v-sheet
-        class="d-flex"
-        :elevation="1"
-    >
-        <div
-            class="d-inline-flex ma-2"
-        >
-            <v-img
-                :width="240"
-                aspect-ratio="4/3"
-                src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-            ></v-img>
-        </div>
-        <div
-            class="d-flex flex-column ma-2"
-        >
-            <router-link
-                to="/products/foo/bar"
-            >
-                <span
-                    class="text-body-1"
-                >
-                    Foo
-                </span>
-            </router-link>
-            <ProductPrice />
-        </div>
-    </v-sheet>
+    <router-link to="/products/foo/bar">
+        <v-sheet class="d-flex" :elevation="1">
+            <div class="d-inline-flex ma-2">
+                <v-img
+                    :width="240"
+                    aspect-ratio="4/3"
+                    src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+                ></v-img>
+            </div>
+            <div class="d-flex flex-column ma-2">
+                <span class="text-body-1"> Foo </span>
+                <ProductPrice />
+            </div>
+        </v-sheet>
+    </router-link>
 </template>
 
 <script lang="ts" setup>
-    import ProductPrice from './ProductPrice.vue';
+import ProductPrice from './ProductPrice.vue'
 </script>
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,19 +1,16 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      MiSArch Frontend
-    </v-app-bar-title>
+    <v-app-bar flat>
+        <v-app-bar-title
+            ><router-link to="/products/foo/bar">
+                MiSArch Frontend
+            </router-link></v-app-bar-title
+        >
 
-    <v-btn
-      prepend-icon="mdi-plus"
-      v-if="true"
-    >
-      Add Product
-    </v-btn>
-    <AddProductDialog />
-  </v-app-bar>
+        <v-btn prepend-icon="mdi-plus" v-if="true"> Add Product </v-btn>
+        <AddProductDialog />
+    </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-  import AddProductDialog from '@/components/AddProductDialog.vue';
+import AddProductDialog from '@/components/AddProductDialog.vue'
 </script>
