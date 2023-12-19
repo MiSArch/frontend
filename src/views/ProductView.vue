118d4bd23b0990c1414504885ce17c796da0d7b1
Leonard Esser <leonardesser.utility@posteo.net>
2023-12-19 16:49:32 +0100

    Refine the product view

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -17,7 +17,7 @@
         <v-divider></v-divider>
         <div class="d-flex flex-column ga-4">
             <div class="d-flex mx-4 ga-4">
-                <div>
+                <v-sheet border>
                     <v-carousel cycle show-arrows="hover">
                         <v-carousel-item
                             :width="360"
@@ -35,39 +35,67 @@
                             src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
                         ></v-carousel-item>
                     </v-carousel>
-                </div>
-                <div class="d-flex flex-column ga-4">
-                    <v-card title="Nikon F2" text="Product Summary">
-                        <div class="d-flex flex-wrap mx-4 mb-4 ga-2">
-                            <v-chip>Photography</v-chip>
-                            <v-chip>Cameras</v-chip>
-                            <v-chip>Film Cameras</v-chip>
+                </v-sheet>
+                <v-card class="align-self-start" elevation="4">
+                    <v-card-item>
+                        <v-card-title>{{ internalName }}</v-card-title>
+                        <v-card-subtitle>{{
+                            productVariantName
+                        }}</v-card-subtitle>
+                    </v-card-item>
+                    <v-card-text>
+                        <div class="d-flex flex-wrap ga-2">
+                            <v-chip>Clothing</v-chip>
+                            <v-chip>T-Shirts</v-chip>
                         </div>
-                        <v-select class="mx-4"
-                            label="Product Variant"
-                            :items="['Nikon F2 50BK', 'Nikon F2 24S', 'Nikon F2 50S']"
-                            variant="underlined"
+                    </v-card-text>
+                    <v-divider></v-divider>
+                    <div class="pa-4">
+                        <v-select
+                            :items="['Black', 'White']"
+                            density="compact"
+                            label="Color"
+                            variant="outlined"
                         ></v-select>
-                        <div class="d-flex flex-wrap mx-4 mb-4 ga-2">
-                            <v-chip>Lens: 50 mm F/1.8</v-chip>
-                            <v-chip>Color: Black</v-chip>
-                        </div>
-                    </v-card>
-                </div>
-                <v-card class="align-self-start">
-                    <ProductPrice :price="90" :retail-price="100" />
-                    <v-select
-                        class="ma-2"
-                        :items="['1', '2', '3', '4', '5']"
-                        density="compact"
-                        hint="Choose how many to add to the cart."
-                        label="Amount"
-                        persistent-hint
-                        variant="solo"
-                    ></v-select>
+                        <v-select
+                            :items="['S', 'M', 'L']"
+                            density="compact"
+                            label="Size"
+                            variant="outlined"
+                        ></v-select>
+                    </div>
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn disabled prepend-icon="mdi-cart">Add To Cart</v-btn>
+                        <v-btn>Reset To Default</v-btn>
+                    </v-card-actions>
+                </v-card>
+                <v-card class="align-self-start">
+                    <v-card-item>
+                        <v-card-title>{{ price }} EUR</v-card-title>
+                        <v-card-subtitle
+                            class="text-decoration-line-through"
+                            v-if="price != retailPrice"
+                        >
+                            {{ retailPrice }} EUR
+                        </v-card-subtitle>
+                    </v-card-item>
+                    <v-divider></v-divider>
+                    <v-card-text>
+                        <v-select
+                            class=""
+                            :items="['1', '2', '3', '4', '5']"
+                            density="compact"
+                            hint="Choose how many to add to the cart."
+                            label="Amount"
+                            persistent-hint
+                            variant="solo"
+                        ></v-select>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn disabled prepend-icon="mdi-cart"
+                            >Add To Cart</v-btn
+                        >
                     </v-card-actions>
                 </v-card>
                 <v-spacer></v-spacer>
@@ -79,8 +107,9 @@
             </div>
             <v-card
                 class="mx-4"
+                elevation="4"
                 title="Description"
-                text="Foo is the single most amazing thing on earth."
+                text="This T-Shirt is the single most amazing T-Shirt on earth."
             >
             </v-card>
         </div>
@@ -89,4 +118,10 @@
 
 <script setup lang="ts">
 import ProductPrice from '@/components/ProductPrice.vue'
+import { ref } from 'vue'
+
+const internalName = ref('New York T-Shirt')
+const productVariantName = ref('New York T-Shirt Black Medium')
+const price = ref(90)
+const retailPrice = ref(100)
 </script>
