ae1fe76af891559cf04a3d7a41666e796f016bc2
Leonard Esser <leonardesser.utility@posteo.net>
2023-12-19 12:32:54 +0100

    Add logic to open and close Add Product dialog

--- src/components/AddProductDialog.vue
+++ src/components/AddProductDialog.vue
@@ -1,90 +1,48 @@
 <template>
-    <v-dialog
-        v-model="addProductDialogIsActive"
-        fullscreen
-        scrollable
-        transition="dialog-bottom-transition"
-    >
+    <v-dialog fullscreen scrollable transition="dialog-bottom-transition">
         <v-card>
-            <v-toolbar
-                dark
-                color="primary"
-            >
-                <v-btn
-                    dark
-                    icon
-                    @click="addProductDialogIsActive = false"
-                >
+            <v-toolbar dark color="primary">
+                <v-btn dark icon @click="$emit('close-dialog')">
                     <v-icon>mdi-close</v-icon>
                 </v-btn>
                 <v-toolbar-title>Add Product</v-toolbar-title>
                 <v-spacer></v-spacer>
                 <v-toolbar-items>
-                    <v-btn>Add Product</v-btn>
+                    <v-btn @click="$emit('close-dialog')">Add Product</v-btn>
                 </v-toolbar-items>
             </v-toolbar>
-            <div
-                class="d-flex flex-column pa-4"
-            >
-                <v-switch
-                    label="Visible"
-                ></v-switch>
-                <v-text-field
-                    clearable
-                    label="Product Name"
-                >
-                </v-text-field>
+            <div class="d-flex flex-column pa-4">
+                <v-switch label="Visible"></v-switch>
+                <v-text-field clearable label="Product Name"> </v-text-field>
                 <v-select
                     density="compact"
                     label="Default Product Variant"
                     :items="['Bar', 'Baz', 'Zep']"
                     variant="solo"
                 ></v-select>
-                <v-tabs
-                    v-model="tabs"
-                >
-                    <v-tab
-                        v-for="n in variants"
-                        :key="n"
-                        :value="'tab-' + n"
-                    >
+                <v-tabs v-model="tabs">
+                    <v-tab v-for="n in variants" :key="n" :value="'tab-' + n">
                         Product Variant {{ n }}
                     </v-tab>
                 </v-tabs>
-                <v-window
-                    v-model="tabs"
-                >
+                <v-window v-model="tabs">
                     <v-window-item
                         v-for="n in variants"
                         :key="n"
                         :value="'tab-' + n"
                     >
-                        <v-card
-                            class="pa-8"
-                            flat
-                        >
-                            <v-switch
-                                label="Visible"
-                            ></v-switch>
+                        <v-card class="pa-8" flat>
+                            <v-switch label="Visible"></v-switch>
                             <v-text-field
                                 clearable
                                 label="Product Variant Name"
                             >
                             </v-text-field>
-                            <v-textarea
-                                clearable
-                                label="Description"
-                            >
+                            <v-textarea clearable label="Description">
                             </v-textarea>
-                            <v-text-field
-                                clearable
-                                label="Retail Price"
-                            >
+                            <v-text-field clearable label="Retail Price">
                             </v-text-field>
-                            <v-text-field
-                                clearable
-                                label="Actual Price"
-                            >
+                            <v-text-field clearable label="Actual Price">
                             </v-text-field>
                             <v-select
                                 label="Categories"
@@ -103,9 +61,9 @@
                                 variant="outlined"
                             ></v-file-input>
                             <v-card-actions>
-                                <v-btn
-                                    prepend-icon="mdi-close"
-                                >Remove Product Variant</v-btn>
+                                <v-btn prepend-icon="mdi-close"
+                                    >Remove Product Variant</v-btn
+                                >
                             </v-card-actions>
                         </v-card>
                     </v-window-item>
@@ -116,10 +74,8 @@
 </template>
 
 <script lang="ts" setup>
-    import { ref } from "vue";
+import { ref } from 'vue'
 
-    const addProductDialogIsActive = ref(true);
-
-    const tabs = ref(true);
-    const variants = ref(3);
+const tabs = ref(true)
+const variants = ref(3)
 </script>
--- src/layouts/default/TheAppBar.vue
+++ src/layouts/default/TheAppBar.vue
@@ -6,11 +6,29 @@
             </router-link></v-app-bar-title
         >
 
-        <v-btn prepend-icon="mdi-plus" v-if="true"> Add Product </v-btn>
+        <v-btn disabled prepend-icon="mdi-cart"> Shopping Cart </v-btn>
+        <v-btn prepend-icon="mdi-plus" @click="openAddProductDialog">
+            Add Product
+        </v-btn>
+        <AddProductDialog
+            v-model="addProductDialogOpen"
+            @close-dialog="closeAddProductDialog"
+        />
         <AddProductDialog />
     </v-app-bar>
 </template>
 
 <script lang="ts" setup>
 import AddProductDialog from '@/components/AddProductDialog.vue'
+import { ref } from 'vue'
+
+const addProductDialogOpen = ref(false)
+
+const openAddProductDialog = () => {
+    addProductDialogOpen.value = true
+}
+
+const closeAddProductDialog = () => {
+    addProductDialogOpen.value = false
+}
 </script>
