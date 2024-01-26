3d1226b1bec711452f6893240b71d7110d52e103
Leonard Esser <leonardesser.utility@posteo.net>
2024-01-26 17:02:46 +0100

    Add extra navigation drawer
    The extra navigation drawer at the right side of a page. Add two new
    views: One for managing products, one for managing categories.
    Initially, the views only hold the two dialogs for adding products and
    categories. Move the app bar buttons for opening the two dialogs to
    the new views. Add the views to the router index.

---
+++ src/views/ManageProductsView.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-app-bar density="compact">
+        <v-spacer></v-spacer>
+        <v-btn prepend-icon="mdi-plus" @click="openAddProductDialog">
+            Add Product
+        </v-btn>
+        <AddProductDialog
+            v-model="addProductDialogOpen"
+            @close-dialog="closeAddProductDialog"
+        />
+    </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+import AddProductDialog from '@/components/AddProductDialog.vue'
+import { ref } from 'vue'
+
+/**
+ * Whether or not the "ADD PRODUCT" dialog is open.
+ */
+const addProductDialogOpen = ref(false)
+
+/**
+ * Opens the "ADD PRODUCT" dialog.
+ */
+function openAddProductDialog() {
+    addProductDialogOpen.value = true
+}
+
+/**
+ * Closes the "ADD PRODUCT" dialog.
+ */
+function closeAddProductDialog() {
+    addProductDialogOpen.value = false
+}
+</script>
