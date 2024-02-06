d862171660301cd9c45054d63acf69bd4937e1e5
nk-coding <niklask.coding@gmail.com>
2024-02-06 01:41:11 +0100

    basic authentication support

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,8 +1,51 @@
 // Utilities
+import Keycloak from 'keycloak-js'
 import { defineStore } from 'pinia'
+import silentCheckSsoHtmlUrl from '@/assets/silent-check-sso.html?url'
 
 export const useAppStore = defineStore('app', {
     state: () => ({
-        //
+        keycloak: null as Keycloak | null,
+        isLoggedIn: false,
     }),
+    actions: {
+        async initLogin() {
+            const keycloak = new Keycloak({
+                url: '/keycloak',
+                realm: 'Misarch',
+                clientId: 'frontend',
+            })
+
+            try {
+                await keycloak.init({
+                    onLoad: 'check-sso',
+                    silentCheckSsoRedirectUri: silentCheckSsoHtmlUrl,
+                    redirectUri: '/',
+                })
+                this.keycloak = keycloak
+                this.isLoggedIn = keycloak.authenticated ?? false
+                console.log('Token', keycloak.token)
+            } catch (error) {
+                console.error('Failed to initialize adapter:', error)
+            }
+        },
+        async login() {
+            try {
+                await this.keycloak?.login({
+                    redirectUri: '/',
+                })
+            } catch (error) {
+                console.error('Failed to login:', error)
+            }
+        },
+        async logout() {
+            try {
+                await this.keycloak?.logout({
+                    redirectUri: '/',
+                })
+            } catch (error) {
+                console.error('Failed to logout:', error)
+            }
+        },
+    },
 })
