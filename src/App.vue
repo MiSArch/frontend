4676617c2dd37ef8a210516077b3b0ab475c2f5c
Leonard Esser <leonardesser.utility@posteo.net>
2024-02-20 10:09:37 +0100

    Ensure that token gets logged
    Regardless of the environment.

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import { useAppStore } from './store/app'
 
 const store = useAppStore()
 
-const logToken = process.env.NODE_ENV === 'development'
+const tokenIsStillRequiredForTesting = true
+const logToken = process.env.NODE_ENV === 'development' || tokenIsStillRequiredForTesting
 store.initLogin(logToken)
 </script>
